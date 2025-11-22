81477c6b24c8236c7d8323b52a056452e19ead30
Peter Beno <peterb85@hotmail.sk>
2025-11-22 20:15:00 +0100

    refaktoring

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  // SEM NAPÍŠ SVOJU DOMÉNU (bez lomky na konci)
-  // Napr.: 'https://peterbeno.sk' alebo 'https://moje-portfolio.netlify.app'
+  
   site: 'https://laifix.com', 
 
   integrations: [sitemap()],
